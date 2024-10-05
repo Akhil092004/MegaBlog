49e324c883ee28532e3fc50891103813afbe9d3b
Akhil092004 <akhiljain092004@gmail.com>
2024-10-05 17:12:32 +0530

    new push

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../Logo';
 
+//useless comment
+
 function Footer() {
   return (
     <section className="relative overflow-hidden py-10 bg-gray-400 border-t-2 border-t-black">
